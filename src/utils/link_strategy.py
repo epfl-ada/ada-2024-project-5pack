26ad9f38009e8bff97747b2d4fae20381353b60f
timcallens <82453505+timcallens@users.noreply.github.com>
2024-12-17 18:20:07 +0100

    push pas tout

--- src/utils/link_strategy.py
+++ src/utils/link_strategy.py
@@ -1,3 +1,4 @@
+import numpy as np
 def get_click_positions(paths, all_links_dict):
     """
     Get click positions of the paths
@@ -24,3 +25,20 @@ def get_click_positions(paths, all_links_dict):
                             c.append(link['position'])
                         break
     return c
+
+
+def get_probability_link(path_click_positions, threshold = 0.3):
+    """
+    Get probability that this path clicks on the top links
+
+    Parameters:
+    - path_click_positions : A list where we mapped path to their relative positions
+    
+
+    Returns:
+    A probability (float) in [0,1]
+    """
+
+    top_clicks = sum(1 for pos in path_click_positions if pos <= threshold)
+
+    return top_clicks/len(path_click_positions)
